45c37df97d72c83f1c3717b6e39a0195d5f3e842
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-09-26 23:45:45 +0200

    implement POST file handler

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
-'use strict';
-
-var express = require('express');
-var cors = require('cors');
-
-// require and use "multer"...
-
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multer = require('multer')
 var app = express();
 
+
 app.use(cors());
+app.use(cors({ optionSuccessStatus: 200 }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+
+app.post('/api/fileanalyse/', function (req, res) {
+  const upload = multer({
+    dest: 'uploads/',
+    limits: {
+      fileSize: 5242880 //5 MBs
+    }
+  }).single('upfile');
+
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) return res.status(422).json({ 'error': 'file size is too large' })
+    return res.json({ 'name': req.file.originalname, 'type': req.file.mimetype, 'size': req.file.size })
+  })
 });
+
+
+app.listen(process.env.PORT, function () {
+  console.log(`Node.js listening on port ${process.env.PORT}`);
+});
+
+module.exports = app;
